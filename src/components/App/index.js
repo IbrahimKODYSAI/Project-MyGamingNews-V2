a19b3d0bd239f225de1795fc39d621041e5af653
ibrahimKODYSAI <saneda.i@live.fr>
2020-01-23 23:24:47 +0100

    install dependences

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+// == Import : npm
+import React from 'react';
+
+// == Import : local
+import './app.scss';
+
+// == Composant
+const App = () => (
+  <div id="app">
+    Hello world
+  </div>
+);
+
+// == Export
+export default App;
